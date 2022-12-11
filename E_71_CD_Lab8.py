867f5b572a3db5abe28ad9ce05090e052800a55f
Yashkumar jain <jainy6214@gmail.com>
2022-12-11 10:37:25 +0530

    no of quotes found in the corpus

--- E_71_CD_Lab8.py
+++ E_71_CD_Lab8.py
@@ -6,6 +6,8 @@ for x in f:
         count_the+=1
 print("Number of the pronoun 'the' in the corpus is : ",count_the)
 f.close()
+
+
 fin = open("task1.txt", "rt",encoding="utf8")
 #read file contents to string
 data = fin.read()
@@ -19,3 +21,9 @@ fin = open("task1.txt", "wt",encoding="utf8")
 fin.write(data)
 #close the file
 fin.close()
+
+f = open("task1.txt", "r", encoding="utf8")
+data=f.read()
+no_quotes=re.findall('“(.+?)”',data)
+print("Number of the quotes in the corpus is : ",len(no_quotes))
+f.close()
