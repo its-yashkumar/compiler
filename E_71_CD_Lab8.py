06f37054acc3ec70b6c0306e0746d5a33623c328
Yashkumar jain <jainy6214@gmail.com>
2022-12-11 10:09:53 +0530

    changes done to capitlize pronoun i

--- E_71_CD_Lab8.py
+++ E_71_CD_Lab8.py
@@ -1,6 +1,21 @@
 import re
 f = open("task1.txt", "r", encoding="utf8")
+count_the=0
 for x in f:
-    y=(re.search('the',x))
-    print(y)
+    if(re.search('the',x)): 
+        count_the+=1
+print("Number of the pronoun 'the' in the corpus is : ",count_the)
 f.close()
+fin = open("task1.txt", "rt",encoding="utf8")
+#read file contents to string
+data = fin.read()
+y=(re.search('^i$',data))
+#replace all occurrences of the required string
+if not y == None:
+     data = data.replace(y, 'python')
+fin.close()
+fin = open("task1.txt", "wt",encoding="utf8")
+#overrite the input file with the resulting data
+fin.write(data)
+#close the file
+fin.close()
